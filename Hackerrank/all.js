3756e0b803b14c1931ec46f976ae5322b453cd4e
aradhya2607 <aradhyashant@gmail.com>
2023-07-20 15:19:56 +0530

    Hackerrank data

---
+++ Hackerrank/all.js
@@ -0,0 +1,67 @@
+Problem.insertMany([
+    //greedy
+    {name: 'Beaytiful Pairs',link: 'https://www.hackerrank.com/challenges/beautiful-pairs?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'greedy' ]},
+    {name: 'Mark and Toys',link: 'https://www.hackerrank.com/challenges/mark-and-toys?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'greedy' ]},
+    {name: 'Max Min',link: 'https://www.hackerrank.com/challenges/angry-children?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'greedy' ]},
+    {name: 'Cloudy Day',link: 'https://www.hackerrank.com/challenges/cloudy-day?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'greedy' ]},
+    {name: 'Fighting Pits',link: 'https://www.hackerrank.com/challenges/fighting-pits?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'greedy' ]},
+    {name: 'Team Formation',link: 'https://www.hackerrank.com/challenges/team-formation?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'greedy' ]},
+    //strings
+    {name: 'Pangrams',link: 'https://www.hackerrank.com/challenges/pangrams?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'strings' ]},
+    {name: 'Funny String',link: 'https://www.hackerrank.com/challenges/funny-string?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'strings' ]},
+    {name: 'Common Child',link: 'https://www.hackerrank.com/challenges/common-child?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'strings' ]},
+    {name: 'Bear and Steady Gene',link: 'https://www.hackerrank.com/challenges/bear-and-steady-gene?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'strings' ]},
+    {name: 'Count Strings',link: 'https://www.hackerrank.com/challenges/count-strings?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'strings' ]},
+    {name: 'Build a String',link: 'https://www.hackerrank.com/challenges/build-a-string?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'strings' ]},
+    //graph theory
+    {name: 'Even Tree',link: 'https://www.hackerrank.com/challenges/even-tree?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'graphs' ]},
+    {name: 'Clique',link: 'https://www.hackerrank.com/challenges/clique?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'graphs' ]},
+    {name: 'Crab Graphs',link: 'https://www.hackerrank.com/challenges/crab-graphs?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'graphs' ]},
+    {name: 'Coprime Paths',link: 'https://www.hackerrank.com/challenges/coprime-paths?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'graphs' ]},
+    {name: 'Computer Game',link: 'https://www.hackerrank.com/challenges/computer-game?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'graphs' ]},
+    //implementation
+    {name: 'Apple and Orange',link: 'https://www.hackerrank.com/challenges/apple-and-orange?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'implementation' ]},
+    {name: 'Between two Sets',link: 'https://www.hackerrank.com/challenges/between-two-sets?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'implementation' ]},
+    {name: 'The Time in Words',link: 'https://www.hackerrank.com/challenges/the-time-in-words?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'implementation' ]},
+    {name: 'The Grid Search',link: 'https://www.hackerrank.com/challenges/the-grid-search?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'implementation' ]},
+    {name: 'Matrix Layer Rotation',link: 'https://www.hackerrank.com/challenges/matrix-rotation-algo?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'implementation' ]},
+    //dp
+    {name: 'Cut Tree',link: 'https://www.hackerrank.com/challenges/cuttree?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'dp' ]},
+    {name: 'Fair Cut',link: 'https://www.hackerrank.com/challenges/fair-cut?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'dp' ]},
+    {name: 'Equal',link: 'https://www.hackerrank.com/challenges/equal?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'dp' ]},
+    {name: 'Angry Children 2',link: 'https://www.hackerrank.com/challenges/angry-children-2?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'dp' ]},
+    {name: 'Brick Tiling',link: 'https://www.hackerrank.com/challenges/brick-tiling?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'dp' ]},
+    //sorting
+    {name: 'Big Sorting',link: 'https://www.hackerrank.com/challenges/big-sorting?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'sortings' ]},
+    {name: 'Closest Numbers',link: 'https://www.hackerrank.com/challenges/closest-numbers?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'sortings' ]},
+    {name: 'The Full Counting Sort',link: 'https://www.hackerrank.com/challenges/countingsort4?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'sortings' ]},
+    {name: "Lily's Homework",link: 'https://www.hackerrank.com/challenges/lilys-homework?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'sortings' ]},
+    {name: 'Insertion Sort',link: 'https://www.hackerrank.com/challenges/insertion-sort?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'sortings' ]},
+    //array
+    {name: 'Dynamic Array',link: 'https://www.hackerrank.com/challenges/dynamic-array?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'array' ]},
+    {name: 'Left Rotation',link: 'https://www.hackerrank.com/challenges/array-left-rotation?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'array' ]},
+    {name: 'Sparse Arrays',link: 'https://www.hackerrank.com/challenges/sparse-arrays?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'array' ]},
+    {name: 'Array Manipulation',link: 'https://www.hackerrank.com/challenges/crush?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'array' ]},
+    //trees
+    {name: 'Preorder Traversal',link: 'https://www.hackerrank.com/challenges/tree-preorder-traversal?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'trees' ]},
+    {name: 'Top View',link: 'https://www.hackerrank.com/challenges/tree-top-view?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'trees' ]},
+    {name: 'Swap Nodes [Algo]',link: 'https://www.hackerrank.com/challenges/swap-nodes-algo?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'trees' ]},
+    {name: 'Is This a Binary Search Tree',link: 'https://www.hackerrank.com/challenges/is-binary-search-tree?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'trees' ]},
+    {name: 'Tree Cordinates',link: 'https://www.hackerrank.com/challenges/tree-coordinates?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'trees' ]},
+    {name: 'Array Pairs',link: 'https://www.hackerrank.com/challenges/array-pairs?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'trees' ]},
+    //Balanced trees
+    
+    {name: 'Self Balancing Tree',link: 'https://www.hackerrank.com/challenges/self-balancing-tree?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'btrees' ]},
+    {name: 'Meadian Updates',link: 'https://www.hackerrank.com/challenges/median?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'btrees' ]},
+    {name: 'Array and Simple Queries',link: 'https://www.hackerrank.com/challenges/array-and-simple-queries?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'btrees' ]},
+    //math
+    {name: 'Army Game',link: 'https://www.hackerrank.com/challenges/game-with-cells?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Connecting Towns',link: 'https://www.hackerrank.com/challenges/connecting-towns?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Best Divisor',link: 'https://www.hackerrank.com/challenges/best-divisor?isFullScreen=true',level: 1,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Is Fibo',link: 'https://www.hackerrank.com/challenges/is-fibo?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Bus Station',link: 'https://www.hackerrank.com/challenges/bus-station?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Twins',link: 'https://www.hackerrank.com/challenges/twins?isFullScreen=true',level: 2,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Help Mike',link: 'https://www.hackerrank.com/challenges/help-mike?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Dancing In Pairs',link: 'https://www.hackerrank.com/challenges/dance-class?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'math' ]},
+    {name: 'Strange Numbers',link: 'https://www.hackerrank.com/challenges/strange-numbers?isFullScreen=true',level: 3,website: 'hackerrank',topics: [ 'math' ]},
+])
